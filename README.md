bca1af0ed535d8e395cefac37af899e76a3f29ab
aethefurry <126417832+aethefurry@users.noreply.github.com>
2023-10-19 21:21:07 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # discord-vrchat-presence
  a vrchat osc and discord rpc server that match up, very scuffed
+
+only works with spotify right now
+*Tested on windows 10 22H2 - Python 3.11*
+
+setup
+
+change discord rpc id
+set spotify free or premium
---
+++ discord.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+import ctypes
+import psutil
+from pypresence import Presence
+import time
+from datetime import datetime
+import GPUtil
+import asyncio
+
+print(f"Started Discord RPC Server: https://github.com/aethefurry/discord-vrchat-presence")
+
+client_id = 'PUT YOU RPC ID HERE'
+#https://discord.com/developers or create one with this link
+
+RPC = Presence(client_id, pipe=0)
+RPC.connect()
+
+global isPlaying
+isPlaying = False
+
+from winsdk.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager
+from winsdk.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionPlaybackStatus  as PlaybackStatus
+
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+
+    current_session = sessions.get_current_session()
+    if current_session:
+        info = await current_session.try_get_media_properties_async()
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+        info_dict['genres'] = list(info_dict['genres'])
+
+        return info_dict
+    else:
+        pass
+
+def get_titles(): 
+    EnumWindows = ctypes.windll.user32.EnumWindows
+    EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+    GetWindowText = ctypes.windll.user32.GetWindowTextW
+    GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+    IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+    
+    global titles
+    titles = []
+    def foreach_window(hwnd, lParam):
+        if IsWindowVisible(hwnd):
+            length = GetWindowTextLength(hwnd)
+            buff = ctypes.create_unicode_buffer(length + 1)
+            GetWindowText(hwnd, buff, length + 1) 
+            titles.append(buff.value)
+        return True
+    EnumWindows(EnumWindowsProc(foreach_window), 0)
+    return titles
+    
+        
+
+if __name__ == '__main__':
+    while True:
+        current_media_info = asyncio.run(get_media_info())
+
+        now = datetime.now().strftime("%H:%M")
+        cpu_per = round(psutil.cpu_percent(), 1)
+        mem = psutil.virtual_memory()
+        mem_per = round(psutil.virtual_memory().percent, 1)
+        GPUs = GPUtil.getGPUs()
+        load = round(GPUs[0].load * 100, 1)
+
+        titles = get_titles()
+
+        title_combo = ""
+        for title in titles:
+            title_combo = title_combo + title
+            
+        if "Spotify Free" in title_combo:
+            isPlaying = False
+        if "Spotify Free" not in title_combo:
+            isPlaying = True
+
+        title_combo = ""
+
+        if isPlaying == False:
+            song="⏸️ Paused"
+        elif isPlaying == True:
+            song = "▶ "+current_media_info.get("title") + " - " + current_media_info.get("artist")
+                            
+        RPC.update(details="RAM: " + str(mem_per) + "% | " + "GPU: " + str(load) + "% | " + "CPU: " + str(cpu_per) + "%" + " | " + str(now), state=str(song))  # Set the presence
+        print(isPlaying)
+
+        time.sleep(15)
